ccf2c41be48823afffdb8ce7b250457b3124aa05
I Dewa Gede Ngr Bramasta D <68767@fif.co.id>
2024-10-02 08:16:25 +0700

    fixing logic bisnis

--- SimpleTipCalculatorApp/app/src/main/java/com/example/simpletipcalculatorapp/MainActivity.kt
+++ SimpleTipCalculatorApp/app/src/main/java/com/example/simpletipcalculatorapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import com.example.simpletipcalculatorapp.R
 
@@ -36,7 +37,7 @@ class MainActivity : ComponentActivity() {
 fun calculateTip(billAmount: EditText, totalTipCost: TextView, totalCost: TextView, tipPercentage: Double) {
     val billAmountText = billAmount.text.toString()
     if (billAmountText.isEmpty()) {
-        return
+        Toast.makeText(billAmount.context, "Please Input Your Bill", Toast.LENGTH_SHORT).show()
     } else {
         val billAmountTotal = billAmountText.toDouble()
         val tipAmount = billAmountTotal * tipPercentage
